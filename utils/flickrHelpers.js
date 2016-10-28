7def03a3ef7347acd6d803ec2adc292676664cd1
Matthew Benjamin <matt.g.benjamin@gmail.com>
2016-10-28 10:08:09 -0500

    feat: parse flickr response data

--- utils/flickrHelpers.js
+++ utils/flickrHelpers.js
@@ -1,24 +1,56 @@
 // flickrHelpers.js
 import axios from 'axios'
 
-// TODO: replace w/ es6 template string
-let flickrURL = 'https://api.flickr.com/services/rest/' +
-  '?method=flickr.photos.search&' +
+// TODO: replace w/ es6 template string? make CONST?
+let flickrURL = 'https://api.flickr.com/services/rest/?' +
+  'method=flickr.photos.search&' +
   'api_key=da4d5964521318630a3613ab6d27f2c9&' +
   'nojsoncallback=1&' +
   'format=json&' +
+  'per_page=20&' +
   'text=';
+
+const makeImgURL = (farmID, serverID, id, secret) => {
+  return `https://farm${farmID}.staticflickr.com/${serverID}/${id}_${secret}.jpg`;
+};
+
+const makeImgLink = (owner, id) => {
+  return `https://www.flickr.com/photos/${owner}/${id}`;
+};
+
+const parseResponse = (responseData) => {
+  let metaData = {
+    page: responseData.page,
+    pages: responseData.pages,
+    perpage: responseData.perpage,
+    total: responseData.total
+  };
+
+  let photoResults = responseData.photo.map(function(photoInfo) {
+    return {
+      // TODO: what about img sizes?
+      imgURL: makeImgURL(photoInfo.farm,
+                         photoInfo.server,
+                         photoInfo.id,
+                         photoInfo.secret),
+      imgLink: makeImgLink(photoInfo.owner, photoInfo.id),
+      id: photoInfo.id,
+      title: photoInfo.title
+    };
+  });
+
+  return { photoResults, metaData };
+}
 
 const flickrHelpers = {
   performSearch: function (queryTerm) {
     let queryURL = flickrURL + queryTerm;
     return axios.get(queryURL)
       .then(function(response) {
-        return response.data.photos;
+        return parseResponse(response.data.photos);
       })
       .catch(function(err) {
-        // TODO: replace w/ template string;
-        console.log('Error in Flickr request: ', err);
+        console.warn('Error in Flickr request: ', err);
       });
   }
 };
